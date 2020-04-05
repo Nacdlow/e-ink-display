853595719952f6d71853ef6226bf4fa8e0ea6160
Humaid AlQassimi <git@humaidq.ae>
2020-04-05 19:05:43 +0400

    Add images, finalise design and pull data from simulation.

---
+++ graphics/README.md
@@ -0,0 +1,12 @@
+## About bitmap icons
+
+All the bitmap images are modified icons from Font Awesome, which are released
+under the Creative Commons Attribution 4.0 International license.
+
+https://fontawesome.com/license/free
+
+### Modifications
+
+The bitmap images are derived from SVG images, resized to fit the low
+resolution of our e-ink display. Battery icons have been rotated 90 degrees
+counter-clockwise.
--- iglu.py
+++ iglu.py
@@ -18,7 +18,49 @@ logging.basicConfig(level=logging.DEBUG)
 font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
 font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
 
+import spidev as SPI
+import collections
+
+import urllib.request
+import ssl
+import json
+import socket
+from time import sleep, gmtime, strftime
+
+# Ignore SSL certificate
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = "https://serow.alq/sim/data.json"
+SimData = collections.namedtuple('sim_data', ['generation', 'usage', 'battery'])
+
+def getData():
+    jsonData = urllib.request.urlopen(url, context=ctx).read()
+    parsedData = json.loads(jsonData)
+
+    batteryMax = parsedData["home"]["solar_max_power"]
+    currBattery = round(parsedData["home"]["battery_store"], 2)
+    usage = round(parsedData["home"]["power_con_rate"], 2)
+    generation = round(parsedData["home"]["power_gen_rate"], 2)
+
+    if currBattery < 0:
+        currBattery = 0
+
+    batteryPercentage = round((currBattery / batteryMax) * 100)
+
+    # print("Battery Capacity: " + str(batteryMax) + " kWh")
+    print("Current Store: " + str(currBattery) + " kWh")
+    print("Battery Percentage: " + str(batteryPercentage) + "%")
+    print("Power Usage: " + str(usage) + " kWh")
+    print("Power Generation: " + str(generation) + " kWh")
+    return SimData(generation = int(generation), usage = int(usage),
+            battery = int(batteryPercentage))
+
+
 def makeImage(batPerc, con, gen):
+    logging.info("Loading data from sim...")
+    dat = getData()
     logging.info("Drawing image")
     image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
     draw = ImageDraw.Draw(image)
@@ -30,9 +72,9 @@ def makeImage(batPerc, con, gen):
     image.paste(solar, (10,40))
     image.paste(bolt, (120,40))
     image.paste(bat, (200,40))
-    draw.text((205, 85), str(batPerc) +'%', font = font15, fill = 0)
-    draw.text((110, 85), str(con) +'kW', font = font15, fill = 0)
-    draw.text((15, 85), str(gen) + 'kW', font = font15, fill = 0)
+    draw.text((205, 85), str(dat.battery) +'%', font = font15, fill = 0)
+    draw.text((110, 85), str(dat.generation) +'kW', font = font15, fill = 0)
+    draw.text((15, 85), str(dat.usage) + 'kW', font = font15, fill = 0)
     draw.text((120, 105), 'local.nacdlow.com', font = font15, fill = 0)
 
     draw.text((205, 20), 'Bat', font = font15, fill = 0)
@@ -60,7 +102,8 @@ try:
 
     while True:
         image = makeImage(batPerc, 11, 40)
-        epd.displayPartial(epd.getbuffer(image))
+        buf = epd.getbuffer(image)
+        epd.displayPartial(buf)
         print("waiting")
         batPerc += 10
         time.sleep(3)
@@ -77,4 +120,3 @@ except KeyboardInterrupt:
     logging.info("Exiting...")
     waveshare.epdconfig.module_exit()
     exit()
-
